41f04d5949f15709c82e8ccdc774f140e6e014b8
EshiniAmarasinghe <eshinib@gmail.com>
2026-09-21 20:24:17 +0530

    feat: add initial CampusEats task list

---
+++ src/tasks.js
@@ -0,0 +1,8 @@
+src/tasks.js 
+// CampusEats task list 
+const tasks = [ 
+  "Design the menu screen", 
+  "Build the orders API", 
+  "Add user login", 
+]; 
+console.log(`CampusEats has ${tasks.length} open tasks`);
